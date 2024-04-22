83835c654debc2c213f9bbf3b379b290b2dc280c
syimleang <syimleang@myseneca.ca>
2024-04-22 13:30:45 -0400

    Add error handling and separate functions for maintainability

--- script.js
+++ script.js
@@ -1,45 +1,70 @@
-const inputBox = document.getElementById('input-box');
+const inputBox = document.getElementById("input-box");
 const listContainer = document.getElementById("list-container");
 
-const addTask =() => {
+const addTask = () => {
+  try {
     // Input value checks
-    if(inputBox.value === ''){
-        alert("You must type something!");
-    }
-    else {
-        const li = document.createElement("li");
-        li.innerHTML = inputBox.value;
-        listContainer.appendChild(li);
-        const span = document.createElement("span");
-        span.innerHTML = "\u00d7";
-        li.appendChild(span);
+    if (inputBox.value === "") {
+      throw new Error("You must type something!");
+    } else {
+      createTask(inputBox.value);
+      saveData();
     }
     // Reset the input value
-    inputBox.value = '';
-    saveData();
-}
+    inputBox.value = "";
+  } catch (error) {
+    alert(error.message);
+  }
+};
 
-// Check the clicked element
-listContainer.addEventListener("click", function(e){
+// Create Task
+const createTask = (value) => {
+  const li = document.createElement("li");
+  li.innerHTML = value;
+  listContainer.appendChild(li);
+  const span = document.createElement("span");
+  span.innerHTML = "\u00d7";
+  li.appendChild(span);
+};
+
+// Handle the clicked item
+const handleItemClick = (e) => {
+  try {
     // If click on the task will mark as completed
-    if(e.target.tagName === "LI") {
-        e.target.classList.toggle("checked");
-        saveData();
+    if (e.target.tagName === "LI") {
+      e.target.classList.toggle("checked");
     }
     // If click on the cross sign wil delete the task
     else if (e.target.tagName === "SPAN") {
-        e.target.parentElement.remove();
-        saveData();
+      e.target.parentElement.remove();
     }
-}, false);
+    saveData();
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+listContainer.addEventListener("click", handleItemClick);
 
 // Save all task inside the List Container in the local storage
 const saveData = () => {
+  try {
     localStorage.setItem("data", listContainer.innerHTML);
-}
+  } catch (error) {
+    console.error(error);
+  }
+};
 
 // Fetch the data from the local storage
 const showTask = () => {
-    listContainer.innerHTML = localStorage.getItem("data");
-}
+  try {
+    const savedData = localStorage.getItem("data");
+    if (savedData !== null) {
+      listContainer.innerHTML = savedData;
+    }
+  } catch (error) {
+    console.error(error);
+  }
+};
+
 showTask();
